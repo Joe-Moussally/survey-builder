9c6453e484d4babfe1294902c0b81b7ebea1652c
MJoe111 <joemoussally111@gmail.com>
2022-06-16 20:48:25 +0300

    Adding migration folders

--- laravel-server/database/migrations/2022_06_16_165913_create_values_table.php
+++ laravel-server/database/migrations/2022_06_16_165913_create_values_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateValuesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('values', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('role')->default('0');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
+            $table->string('value');
+            $table->foreignId('question_id')->constrained();
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('values');
     }
 }
